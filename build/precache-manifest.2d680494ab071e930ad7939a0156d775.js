8145de0a78f6565206bc8957c297b55248ec2a89
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-12-19 13:49:42 +0300

    Deployment Ready

---
+++ build/precache-manifest.2d680494ab071e930ad7939a0156d775.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22177af1e82fc874161bed842394662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5220e8ce0c7b01cd5dae",
+    "url": "/static/css/2.f15df562.chunk.css"
+  },
+  {
+    "revision": "c9c7932986c5ce770fb7",
+    "url": "/static/css/main.37210215.chunk.css"
+  },
+  {
+    "revision": "5220e8ce0c7b01cd5dae",
+    "url": "/static/js/2.c83349a4.chunk.js"
+  },
+  {
+    "revision": "a9b1d1a7d6e36848ea12ab0bc77513ad",
+    "url": "/static/js/2.c83349a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9c7932986c5ce770fb7",
+    "url": "/static/js/main.e3976fd2.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  },
+  {
+    "revision": "fbefa09c07bbea0c2f857449f9fa1495",
+    "url": "/static/media/car.fbefa09c.jpg"
+  },
+  {
+    "revision": "ac8b8a6648618864dc9a9ac27d90eec9",
+    "url": "/static/media/logo.ac8b8a66.png"
+  },
+  {
+    "revision": "98cf803effca2c93c0a1a092052d2624",
+    "url": "/static/media/map.98cf803e.png"
+  }
+]);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv/config");
 const PORT = process.env.PORT || 3333;
-const DB = process.env.DB_CONNECT
+const DB = process.env.DB_CONNECT;
 
 mongoose.set("useCreateIndex", true);
 
@@ -20,13 +20,16 @@ mongoose.connect(
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+server.use(express.static(path.join(__dirname, "build")));
 
 //Routes
 server.use("/api/v1/users", require("./routes/users.routes"));
 server.use("/api/v1/items", require("./routes/items.routes"));
 server.use("/api/v1/users/chats", require("./routes/messages.routes"));
 server.use("/admin", require("./routes/admin.routes"));
-
+server.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
